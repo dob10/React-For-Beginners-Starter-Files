ab123d64d32e6d8abd923b3f790ed4d92b79241d
DESKTOP-T16OHFF\Billy <xtkbilly+sourcetree@gmail.com>
2022-06-14 16:17:18 -0500

    lesson 5 and 6

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Inventory extends Component {
+  render() {
+    return (
+      <div>Inventory</div>
+    )
+  }
+}
+
+export default Inventory;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,8 @@
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
+import "./css/style.css";
 
 
 // render(<p>REACT-DOM render</p>, document.querySelector('#main'));
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
