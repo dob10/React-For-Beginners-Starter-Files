9d17c6ed479c3f22b3f1f797d30b4962eec134cc
DESKTOP-T16OHFF\Billy <xtkbilly+sourcetree@gmail.com>
2022-06-14 15:09:59 -0500

    lesson 3 - Creating first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hey');
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+// render(<p>REACT-DOM render</p>, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
